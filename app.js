ee7c5a9d648a4780ab1f6366631e42138150413c
Dr Umesh R Bilagi <umeshbilagi@gmail.com>
2018-02-23 12:28:50 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var lang = require("./language");
+
+var wordHundred = (cNumber, lan)=>{
+
+}
+var millonComa = (cNumber)=>{
+
+}
+
+var lakhComa = (cNumber)=>{
+
+}
+
+var millionWord = (cNumber, lang, currencyStyle=true)=>{
+
+} 
+
+var lakhWord = (cNumber, lang, currencyStyle=true)=>{
+    
+} 
