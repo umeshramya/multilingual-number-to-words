48642d3a6b1b9de98ab4b01012b63f2c7a97b9b0
Umesh Bilagi <umeshbilagi@gmail.com>
2021-06-20 20:32:24 +0530

    done

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-const {NumberToWord, kananda}= require("multilingual-number-to-words")
+const {NumberToWord, kananda, english}= require("multilingual-number-to-words")
 // import {NumberToWord} from "multilingual-number-to-words"
 
 
-let n = new NumberToWord(kananda);
+let n = new NumberToWord(english);
 
-console.log(n.getWord(3));
+console.log(n.getWord(1004));
 console.log(n.convertToComaSeparetedString(1234567890001, "MillionAndBillion"));
 console.log(n.convertToComaSeparetedString(1234567890001 ));
 
