83a799d85ce21616a220ee1a399a46f9177a460c
pradyumnabilagi <pradyumnabilagi12@gmail.com>
2021-06-27 09:31:16 +0530

    deleted test.js file at root

--- test/index.js
+++ test/index.js
@@ -7,4 +7,5 @@ let n = new NumberToWord(english);
 console.log(n.getWord( 123000789111.04,"MillionAndBillion", "Currency"));
 console.log(n.convertToComaSeparetedString(123456789111, "MillionAndBillion"));
 console.log(n.convertToComaSeparetedString(123456789111 ));
+console.log(n.convertToComaSeparetedString(123456.150, "LakhsAndCrore"))
 
